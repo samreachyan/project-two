b5e57b327477cc779c839b6c82f4bcf31d2114b4
samreachyan <samreachyan@gmail.com>
2020-05-28 05:13:17 +0700

    og

--- resources/views/shopping/master/og.blade.php
+++ resources/views/shopping/master/og.blade.php
@@ -1,9 +1,12 @@
+<meta property="og:type" content="website" >
 <meta property="og:title" content="Samreach Online Clothes">
-<meta property="og:description" content="Samreach Demo Website For Online Clothes Project">
-<meta property="og:image" content="http://34.80.90.118/backend/img/logo/logo.png">
-<meta property="og:url" content="http://34.80.90.118/index.html">
+<meta property="og:description" content="Samreach Demo Website For Online Men Women Clothes Website Project">
+<meta property="og:image" content="http://34.80.90.118/backend/img/cover1.jpg">
+<meta property="og:url" content="http://34.80.90.118/">
+<meta property="og:image:width" content="975">
+<meta property="og:image:height" content="650">
 
-<meta name="facebook:title" content="Samreach Men Women Online Clothes">
+{{-- <meta name="facebook:title" content="Samreach Men Women Online Clothes">
 <meta name="facebook:description" content="Samreach Demo Website For Online Men Women Clothes Website Project">
 <meta name="facebook:image" content="http://34.80.90.118/backend/img/cover1.jpg">
-<meta name="facebook:card" content="Samreach Online Clothes Demo">
+<meta name="facebook:card" content="Samreach Online Clothes Demo"> --}}
